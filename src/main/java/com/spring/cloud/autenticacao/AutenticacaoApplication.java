d2e675e9ce46a1b32fafadfeed097af7e167d98d
felipevianna <felipe.vianna86@gmail.com>
2021-04-03 11:15:39 -0300

    #7 - Inserindo eureka client para que a aplicação possa se registrar no server.

--- src/main/java/com/spring/cloud/autenticacao/AutenticacaoApplication.java
+++ src/main/java/com/spring/cloud/autenticacao/AutenticacaoApplication.java
@@ -2,8 +2,10 @@ package com.spring.cloud.autenticacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AutenticacaoApplication {
 
 	public static void main(String[] args) {
